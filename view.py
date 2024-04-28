47e8e8614c2539239b273a40a20a1e354f91eae5
Kazim <b.kazimy@gmail.com>
2024-04-28 11:38:10 +0530

    .env secured

--- view.py
+++ view.py
@@ -12,6 +12,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 # Import your forms from the forms.py
 import forms
+import os
+
+# os.load_env()
 
 
 '''
@@ -28,7 +31,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -60,7 +63,7 @@ def is_authorized():
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
